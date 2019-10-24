50d598c509117f4e6cf621964cafad385ed51e86
Sophie <sophieladder@pm.me>
2019-10-24 11:31:44 -0700

    update 2 2019-10-24, add witness, slight updates

--- print.js
+++ print.js
@@ -1,5 +1,5 @@
 function printtopdf() {
-  if (signaturePad1.isEmpty() ||signaturePad2.isEmpty() || document.getElementById("model1name").value == "MODEL 1 FULL LEGAL NAME" || document.getElementById("model2name").value == "MODEL 2 FULL LEGAL NAME" || document.getElementById("model1addy").value == "MODEL 1 FULL ADDRESS" || document.getElementById("model2addy").value == "MODEL 2 FULL ADDRESS" || document.getElementById("content").value == "NAME/DESCRIBE VIDEO AND/ OR PICTURES" || document.getElementById("locality").value == "LOCALITY") {
+  if (signaturePad1.isEmpty() ||signaturePad2.isEmpty() || document.getElementById("model1name").value == "" || document.getElementById("model2name").value == "" || document.getElementById("model1addy").value == "" || document.getElementById("model2addy").value == "" || document.getElementById("content").value == "" || document.getElementById("locality").value == "") {
     // checks all the fields, if one is empty or at its default value returns an error message instead of creating the PDF
     return alert("Information or signature missing, please review the document then try again.");
   }
@@ -80,13 +80,20 @@ var name ="";
   // selling conditions
   doc.text(xmargin, yline, '6.  I hereby grant the '+ name +' permission to use, reproduce, sell, license, rent or otherwise distribute and publish, modify, edit and alter \nthe Content, except as limited below in 6.1 - 6.6. Furthermore, I hereby agree and authorize the '+ name +' to publish the Content online, \non the platform of their choice, including any Web platform where independent content uploaders can upload, publish, license and sell \ntheir original adult videos, services or other tangibles goods to final users, including all related subdomains or Web sites of such platform \n(all together referred to as the "Platform"), except as limited by 6.1 - 6.6, and such authorization is given for an unlimited time;');
   yline+=20;
+
   if (document.getElementById("minvalue").value == 0){
     doc.text(xmargin+10, yline,'6.1 I hereby grant the '+ name +' permission to sell the Content with no minimum value');
     yline+=5;
   }
   else {
-    doc.text(xmargin+10, yline, '6.1 The '+ name +' shall NOT sell the Content, in whole or in part, for a value of less than $' +  document.getElementById("minvalue").value + ' USD unless given express \nwritten permission by the other party.');
-    yline+=9;
+    if (document.getElementsByName("group1")[0].checked){
+      doc.text(xmargin+10, yline, '6.1 The '+ name +' shall NOT sell the Content, in whole or in part, for a value of less than $' +  document.getElementById("minvalue").value + ' USD unless given express \nwritten permission by the other party.');
+      yline+=9;
+    }
+    else {
+      doc.text(xmargin+10, yline, '6.1 The '+ name +' shall NOT sell the Content, in whole or in part, for a value of less than $' +  document.getElementById("minvalue").value + ' USD within the first\n' + document.getElementById("selltime").value + ' after release unless given express written permission by the other party.');
+      yline+=9;
+    }
   }
   if (document.getElementById("mintime").value == 0){
     doc.text(xmargin+10, yline,'6.2 The ' + name + ' agree(s) to not upload any of the Content to free-to-view services');
@@ -106,11 +113,12 @@ var name ="";
   yline+=13;
 
   doc.text(xmargin, yline, '7. I hereby agree to not disclose the other party’s legal name or address (hereinafter referred to as "Confidential Information"), \nexcept as required by the Platform to sell the Content, or as required by the law, and shall refrain from disclosing such Confidential \nInformation to any third party without prior, written approval from the other party, and shall protect such Confidential Information \nfrom inadvertent disclosure to a third party using the same care and diligence that I use to protect my own confidential information, \nbut in no case less than reasonable care. I shall ensure that each of my employees, officers, directors, or agents who has access to \nConfidential Information disclosed under this Agreement is informed of its confidential nature and is required to abide by the terms \nof this Agreement. I shall promptly notify the affected party of any disclosure of such Confidential Information in violation of this \nAgreement or of any subpoena or other legal process requiring production or disclosure of said Confidential Information;');
-  yline+=34;
-  doc.text(xmargin, yline, '8. I hereby understand and agree that the Content may be searchable by others through the Platform itself and through others \npartnered or networked entities of the Platform or the ' + name + '. Furthermore, I understand and agree that the Content may be searchable \nby publicly available search engines, and it is my sole responsibility should I wish not to appear, be found, or be \nremoved of such search engines; ');
 
   doc.addPage("letter","p");  // filled up first page, create new page
   yline = 15;
+  doc.text(xmargin, yline, '8. I hereby understand and agree that the Content may be searchable by others through the Platform itself and through others \npartnered or networked entities of the Platform or the ' + name + '. Furthermore, I understand and agree that the Content may be searchable \nby publicly available search engines, and it is my sole responsibility should I wish not to appear, be found, or be \nremoved of such search engines; ');
+  yline += 20;
+
   doc.text(xmargin, yline, '9. If the Content is found on a site(s) that is not indicated related to the ' + name + ', or their licensees, as the case may be, I shall \nhave full rights to demand that the Content be taken down from such unapproved site and/or destroyed, the whole at my own costs; ');
   yline+=13;
   doc.text(xmargin, yline, '10. I hereby release, discharge and undertake to indemnify and hold harmless the ' + name + ' and the Platform from and against \nany and all claims, liability, costs, losses, damages or injuries of any kind arising out of or related to my participation in the Content. \nWithout limiting the generality of the foregoing, I agree that the ' + name + ' have neither made nor will be in any manner responsible or \nliable for any warranty, representation or guarantee, express or implied, in fact or in law, in connection with the Content or my participation \nin the Content. I further release all rights to bring any claim, action or proceeding against the ' + name + ' and the Platform.');
@@ -124,25 +132,33 @@ var name ="";
   doc.text(xmargin, yline, '14. I agree and understand that I am solely responsible to comply with all applicable laws and regulations in my location as well as \nall laws and regulations applicable to my participation in the Content.');
   yline+=13;
   doc.text(xmargin, yline, '15. This Agreement shall be governed by and construed in accordance with the laws of ' + document.getElementById('locality').value + '. The parties irrevocably agree \nthat the courts of ' + document.getElementById('locality').value +' shall have exclusive jurisdiction to settle any dispute or claim that arises out of or in connection \nwith this Agreement or its subject matter or formation (including non-contractual disputes or claims).');
-  yline+=20;
+  yline+=25;
 
   // model 1 name and date signed
   doc.text(xmargin, yline, document.getElementById("model1name").value + ' \nRead and accepted this day, ' + document.getElementById('sigdate').value);
   // take the data from the signature pad (an HTML5 canvas), convert it to a png, and insert it into the PDF
   // model 1 signature
   var imgData1 = window.signaturePad1.toDataURL('image/png');
-  doc.addImage(imgData1, 'png', 10, 138, 100, 17);
+  doc.addImage(imgData1, 'png', 10, yline+4, 100, yline-440);
   doc.setLineWidth(0.25);
-  doc.line(12, 155, 100, 155);
+  doc.line(12, yline+22, 100, yline+22);
 
-  yline+=50;
+  yline+=30;
   // model 2 name and date
   doc.text(xmargin, yline, document.getElementById("model2name").value + ' \nRead and accepted this day, ' + document.getElementById('sigdate').value);
 
   // model 2 signature
   var imgData2 = window.signaturePad2.toDataURL('image/png');
-  doc.addImage(imgData2, 'png', 10, 190, 100, 17);
-  doc.line(12, 206, 100, 206);
+  doc.addImage(imgData2, 'png', 10, yline+4, 100, yline-480);
+  doc.line(12, yline+22, 100, yline+22);
+
+  yline+=50;
+  if (document.getElementById("witcheck").checked){
+    doc.text(xmargin, yline, 'Witness: ' + document.getElementById("witness").value);
+    var imgData3 = window.signaturePad3.toDataURL('image/png');
+    doc.addImage(imgData3, 'png', 10, yline+4, 100, yline-520);
+    doc.line(12, yline+22, 100, yline+22);
+  }
 
   // adds another page for the IDs
   doc.addPage("letter","p");
---
+++ script.js
@@ -0,0 +1,191 @@
+var sig1 = document.getElementById('signature1');
+var sig2 = document.getElementById('signature2');
+var sig3 = document.getElementById('signature3');
+var idimg1 = "";
+var idimg2 = "";
+var formtype = "shared";
+
+document.getElementById('form_share').addEventListener('click', function () {
+formtype = "shared";
+document.getElementById("model1name").value = "MODEL 1 FULL LEGAL NAME";
+});
+document.getElementById('form_exclude').addEventListener('click', function () {
+formtype = "exclude";
+document.getElementById("model1name").value = "MODEL 1 (OWNER) FULL LEGAL NAME";
+});
+
+function wit(){
+  var x = document.getElementById("witness");
+  if (document.getElementById('witcheck').checked) {
+    document.getElementById('witness').style.display = "inline";
+    document.getElementById("witness_sig").style.display = "block";
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    sig3.width = sig3.offsetWidth * ratio;
+    sig3.height = sig3.offsetHeight * ratio;
+    sig3.getContext("2d").scale(ratio, ratio);
+  }
+  else {
+    document.getElementById('witness').style.display = "none";
+    document.getElementById("witness_sig").style.display = "none";
+  }
+}
+function tests(){
+  if (document.getElementsByName("group1")[0].checked){
+    alert ("one");
+  }
+  else {
+    alert("twp");
+  }
+}
+// Adjust canvas coordinate space taking into account pixel ratio,
+// to make it look crisp on mobile devices.
+// This also causes canvas to be cleared.
+function resizeCanvas() {
+  // When zoomed out to less than 100%, for some very strange reason,
+  // some browsers report devicePixelRatio as less than 1
+  // and only part of the canvas is cleared then.
+  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+  sig1.width = sig1.offsetWidth * ratio;
+  sig1.height = sig1.offsetHeight * ratio;
+  sig1.getContext("2d").scale(ratio, ratio);
+
+  sig2.width = sig2.offsetWidth * ratio;
+  sig2.height = sig2.offsetHeight * ratio;
+  sig2.getContext("2d").scale(ratio, ratio);
+
+  sig3.width = sig3.offsetWidth * ratio;
+  sig3.height = sig3.offsetHeight * ratio;
+  sig3.getContext("2d").scale(ratio, ratio);
+}
+
+resizeCanvas();
+
+var signaturePad1 = new SignaturePad(sig1, {});
+var signaturePad2 = new SignaturePad(sig2, {});
+var signaturePad3 = new SignaturePad(sig3, {});
+
+document.getElementById('clear').addEventListener('click', function () {
+signaturePad1.clear();
+});
+document.getElementById('clear2').addEventListener('click', function () {
+signaturePad2.clear();
+});
+document.getElementById('clear3').addEventListener('click', function () {
+signaturePad3.clear();
+});
+
+var openFile = function(event) {
+  var input = event.target;
+
+  var reader = new FileReader();
+  reader.onload = function(){
+    var dataURL = reader.result;
+    window.idimg1 = reader.result;
+    var output = document.getElementById('output1');
+    output.src = dataURL;
+  };
+  reader.readAsDataURL(input.files[0]);
+};
+
+var openFile2 = function(event) {
+  var input = event.target;
+
+  var reader2 = new FileReader();
+  reader2.onload = function(){
+    var dataURL2 = reader2.result;
+    window.idimg2 = reader2.result;
+    var output2 = document.getElementById('output2');
+    output2.src = dataURL2;
+  };
+  reader2.readAsDataURL(input.files[0]);
+};
+
+// takes the values the users have entered and fills the form
+function generateForm() {
+
+  if (document.getElementById("model1name").value == "" || document.getElementById("model2name").value == "" || document.getElementById("model1addy").value == "" || document.getElementById("model2addy").value == "" || document.getElementById("content").value == "" || document.getElementById("locality").value == "" || document.getElementById("date").value == "") {
+    // checks all the fields, if one is empty or at its default value returns an error message instead of filling the form
+    return alert("Information missing, please review then try again.");
+  }
+  else {
+    var x = document.getElementById("bg");
+    x.style.display = "block";
+    document.getElementById("fill_1name").innerHTML = document.getElementById("model1name").value;
+    document.getElementById("fill_1add").innerHTML = document.getElementById("model1addy").value;
+    if (formtype == "exclude"){
+      document.getElementById("model1ident").innerHTML = "(hereinafter referred to as the 'OWNER')<br>";
+      document.getElementById("intro").innerHTML = "In favor of";
+      document.getElementById("model2ident").innerHTML = "<br>For good and valuable consideration, the receipt and sufficiency of which is hereby acknowledged, I,";
+      document.getElementById("intro2").innerHTML = "<br>hereby agree as follows: ";
+      document.getElementById("sharetype").innerHTML = "I hereby acknowledge the Content is sole property of OWNER, and that I do not hold any rights to reproduce, sell, license, rent or otherwise distribute and publish, modify, edit and alter the Content unless given written permission by the OWNER;";
+    }
+    else {
+      document.getElementById("model1ident").innerHTML = "<br>AND";
+      document.getElementById("intro").innerHTML = "For good and valuable consideration, the receipt and sufficiency of which is hereby acknowledged, I,";
+      document.getElementById("model2ident").innerHTML = "";
+      document.getElementById("intro2").innerHTML = "<br>hereinafter referred to collectively as the “PARTIES”, and individually as a “PARTY” all hereby agree as follows:"
+      document.getElementById("sharetype").innerHTML = "I hereby acknowledge that ownership of the Content is jointly shared between both Parties, and that I do not hold exclusive rights to  reproduce, sell, license, rent or otherwise distribute and publish, modify, edit and alter the Content;";
+    }
+    document.getElementById("fill_2name").innerHTML = document.getElementById("model2name").value;
+    document.getElementById("fill_2add").innerHTML = document.getElementById("model2addy").value;
+
+    var names = document.getElementsByClassName("name");
+    for(var i = 0; i < names.length; i++)
+    {
+      if (formtype == "shared") {
+        names[i].innerHTML = "the PARTIES";
+      }
+      else {
+        names[i].innerHTML = "the OWNER"
+      }
+    }
+
+    document.getElementById("fill_content").innerHTML = document.getElementById("content").value;
+    document.getElementById("fill_date").innerHTML = document.getElementById("date").value;
+    document.getElementById("fill_locality").innerHTML = document.getElementById("locality").value;
+    document.getElementById("fill_locality2").innerHTML = document.getElementById("locality").value;
+    document.getElementById("fill_minvalue").innerHTML = document.getElementById("minvalue").value;
+    document.getElementById("slength").innerHTML = "for the first " + document.getElementById("selltime").value +" after release";
+
+    // changes the wording of the sale restrictions based on imput
+    if (document.getElementById("mintime").value == 999){
+      if (formtype == "shared"){
+        document.getElementById("minclause").innerHTML = "6.2 Both PARTIES shall be allowed to upload the Content, in whole or in part, to free-to-view services";
+      }
+      else {
+        document.getElementById("minclause").innerHTML = "6.2 The OWNER shall be allowed to upload the Content, in whole or in part, to free-to-view services;"
+      }
+    }
+    else if (document.getElementById("mintime").value == 0){
+      if (formtype == "shared"){
+      document.getElementById("minclause").innerHTML = "6.2 I hereby agree to not upload any of the Content to free-to-view services";
+      }
+      else {
+        document.getElementById("minclause").innerHTML = "6.2 The OWNER hereby agrees not to upload any of the Content to free-to-view services";
+      }
+    }
+    else {
+      if (formtype == "shared"){
+        document.getElementById("minclause").innerHTML = "6.2 No PARTY shall upload more than " + document.getElementById("mintime").value + " minutes of the Content to free-to-view services.";
+      }
+      else {
+        document.getElementById("minclause").innerHTML = "6.2 The OWNER shall not upload more than " + document.getElementById("mintime").value + " minutes of the Content to free-to-view services.";
+      }
+    }
+
+    document.getElementById("fill_bundle").innerHTML = document.getElementById("bundle").value;
+    document.getElementById("fill_giveaway").innerHTML = document.getElementById("giveaway").value;
+    document.getElementById("fill_premium").innerHTML = document.getElementById("premium").value;
+  }
+}
+
+
+// show or hide the selling restrictions at the top of the page
+function strict() {
+  if (document.getElementById('salecheck').checked){
+    document.getElementById('restrictions').style.display = "block";
+  }
+  else {
+    document.getElementById('restrictions').style.display = "none";
+  }
+}
